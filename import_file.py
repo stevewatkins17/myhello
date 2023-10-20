d440fe11a34b440e756b48ddd8b455405e0cdede
Steve Watkins <stevewatkins@Steves-MBP.attlocal.net>
2023-10-19 23:55:53 -0400

    cleanup import file and convert to ipynb

--- import_file.py
+++ import_file.py
@@ -1,17 +1,8 @@
 myfile = "fruit.txt"
 
 with open(myfile, "r") as f:
-    data = f.read()
-
-print(type(data))
-print(data)
-
-with open(myfile, "r") as f:
     data = f.readlines()
 
-print(type(data))
-
 for line in data:
-    print(data.index(line))
-    print(line)
+    print(f"{data.index(line)} " + line) 
 
